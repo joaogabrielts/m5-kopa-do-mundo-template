d2bfe17d12ea7afac29b2af5880e3a133ab81a8c
Gabriel Teixeira <139800510+joaogabrielts@users.noreply.github.com>
2025-04-04 20:55:09 -0300

    feat: tarefa x finalizada

--- kopa_do_mundo/views.py
+++ kopa_do_mundo/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Team
+from .serializers import TeamSerializer
 
-# Create your views here.
+
+class TeamDetailView(APIView):
+    def get(self, request, team_id):
+    
+        team = get_object_or_404(Team, id=team_id)
+        serializer = TeamSerializer(team)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def patch(self, request, team_id):
+        team = get_object_or_404(Team, id=team_id)
+        serializer = TeamSerializer(team, data=request.data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, team_id):
+        team = get_object_or_404(Team, id=team_id)
+        team.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
